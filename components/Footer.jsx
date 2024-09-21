f73178b856488a7eb6d200f04822745382931ca4
iampeterjose <peterjose098@gmail.com>
2024-09-21 12:53:06 +0800

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,43 @@
+import { FaLocationArrow } from "react-icons/fa6"
+import MagicButton from "./ui/MagicButton"
+import { socialMedia } from "@/data"
+
+const Footer = () => {
+    return (
+        <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
+
+            <div className="flex flex-col items-center">
+                <h1 className="heading lg:max-w-[45vw]">
+                    Geared up to transform <span className="text-purple">your</span> online presence?
+                </h1>
+                <p className="text-white-200 md:mt-10 my-5 text-center">Reach out to me today, and let&apos;s collaborate.</p>
+                <a href="mailto:peterjose098@gmail.com">
+                    <MagicButton 
+                        title="Let's get in touch" 
+                        icon={<FaLocationArrow />}
+                        position="right"
+                    />
+                </a>
+            </div>
+
+            <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+                <p className="md:text-base text-sm md:font-nomal font-light">
+                    Copyright &copy; {new Date().getFullYear()} Peter
+                </p>
+
+                <div className="flex items-center md:gap-3 gap-6">
+                    {socialMedia.map((profile) => (
+                        <div 
+                            key={profile.id} 
+                            className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg  border border-black-300"
+                        >
+                            <img src={profile.img} alt={profile.id} width={20} height={20} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ data/index.js
@@ -0,0 +1,119 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I value client collaboration, nurturing clear and open communication.",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm open to adjusting my schedule for seamless communication.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I actively seek to enhance my skills.",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Passionate about development and eager to explore technology.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 6,
+      title: "Are you up for starting a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Online Coffee Shop",
+      des: "Discover the rich and aromatic world of coffee with TestApp, your ultimate destination for premium blends and expertly crafted brews.",
+      img: "/coffeeapp.png",
+      iconLists: ["/next.svg", "/tail.svg", "mongo.svg"],
+      link: "https://testing-lac-eta.vercel.app/",
+    },
+  ];
+  
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
